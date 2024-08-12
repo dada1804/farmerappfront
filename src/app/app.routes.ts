b74d65088c5322e149cd1c72fbe02754bd3e8d7e
Venkatesh A Hanamsagar <venkateshahanamsagar@Venkateshs-Laptop.local>
2024-08-12 11:17:17 +0530

    Initial files for front end of farmerapp

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { CountriesComponent } from './components/countries/countries.component';
+import { FarmersComponent } from './components/farmers/farmers.component';
+import { FarmsComponent } from './components/farms/farms.component';
+import { SchedulesComponent } from './components/schedules/schedules.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'countries', component: CountriesComponent },
+  { path: 'farmers', component: FarmersComponent },
+  { path: 'farms', component: FarmsComponent },
+  { path: 'schedules', component: SchedulesComponent },
+  { path: '', redirectTo: '/countries', pathMatch: 'full' }
+];
